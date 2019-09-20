f1c48f847dee4c8f1b47b98fe769a959544f0814
Naveen Kashyap <naveenkashyap243@gmail.com>
2019-09-21 00:08:05 +0530

    removed redux extension

--- src/store.js
+++ src/store.js
@@ -3,5 +3,5 @@ import thunk from 'redux-thunk';
 import allReducers from './reducers';
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(allReducers,initialState,compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()));
+const store = createStore(allReducers,initialState,compose(applyMiddleware(...middleware)));
 export default store;
