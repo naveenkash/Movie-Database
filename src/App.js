82b53e87324a82c7278e2cd69754fcc3cf11b244
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-08-19 13:15:16 +0530

    change url and add app.js

--- src/App.js
+++ src/App.js
@@ -2,45 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Navbar from "./components/top_navbar/navbar";
-import Signup from "./signup";
+import { connect } from "react-redux";
+import { isLoggedIn } from "../src/actions";
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
-      rt: localStorage.getItem("request_token"),
-      request: false,
     };
+    this.props.isLoggedIn();
   }
   openSideNav = () => {
-    this.setState({ open: true }, () => {
-    });
+    this.setState({ open: true }, () => {});
   };
   closeSideNav = () => {
-    this.setState({ open: false }, () => {
-    });
-  };
-  request = (tokenRequested) => {
-    this.setState({ request: tokenRequested });
-  };
-  close = (close) => {
-    this.setState({ request: close });
+    this.setState({ open: false }, () => {});
   };
   render() {
     return (
       <div className="App">
-        <Navbar clicked={this.openSideNav} tokenRequested={this.request} />
-        {(() => {
-          if (!this.state.request) {
-            return null;
-          } else {
-            return <Signup closeSignup={this.close} />;
-          }
-        })()}
+        <Navbar clicked={this.openSideNav} />
         <Header closeSide={this.closeSideNav} open={this.state.open} />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, { isLoggedIn })(App);
